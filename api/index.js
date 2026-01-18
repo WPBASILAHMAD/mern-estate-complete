41b3632b77ff915440d5dfbbe99b32c4d4d387cc
WPBASILAHMAD <basilmedia7@gmail.com>
2026-01-18 22:47:57 +0500

    All Done

--- api/index.js
+++ api/index.js
@@ -33,6 +33,10 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000!');
+    });
+}
+
+export default app;
